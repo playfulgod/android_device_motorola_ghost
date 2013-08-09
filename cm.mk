84d5845480b338ce3ce68e6ec48d8fe929ffe862
hudson@koushik-lion <jenkins@cyanogenmod.com>
2013-08-09 15:23:45 -0700

    mkvendor.sh: Initial commit of ghost

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := ghost
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/ghost/device_ghost.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ghost
+PRODUCT_NAME := cm_ghost
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := ghost
+PRODUCT_MANUFACTURER := motorola
