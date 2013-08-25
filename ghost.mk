d34465f32ce7eb72ee07da7d698d2ba7fee4dd16
Matthew Groff <mattlgroff@gmail.com>
2013-08-25 07:06:37 -0700

    Removing touch_dev.idc (since it didn't work for OUDhs) and marking this as WORKING.
    Signed-off-by: Matthew Groff <mattlgroff@gmail.com>

--- ghost.mk
+++ ghost.mk
@@ -17,10 +17,6 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-PRODUCT_COPY_FILES += \
-	device/motorola/ghost/touch_dev.idc:system/usr/idc/touch_dev.idc	
-	
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
