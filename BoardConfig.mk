2baac643d2825bc58e528d850586998856a93d7e
PlayfulGod <playfulgod@gmail.com>
2013-12-15 00:10:34 -0500

    bringup for cwm on 4.4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,19 +5,20 @@ USE_CAMERA_STUB := true
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := msm8960
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
+
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := ghost
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 vmalloc=400M
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 vmalloc=400M androidboot.write_protect=0 zcache androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 
-#Thanks to JackpotClavin for bringing this point up in http://forum.xda-developers.com/showthread.php?p=44884904#post44884904
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02200000 
 
 # fix this up by examining /proc/mtd on a running device
@@ -31,5 +32,7 @@ TARGET_PREBUILT_KERNEL := device/motorola/ghost/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-#Font size too small for 720p. Fixing.
+# Recovery
+RECOVERY_FSTAB_VERSION = 2
+TARGET_RECOVERY_FSTAB = device/motorola/ghost/fstab.qcom
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
